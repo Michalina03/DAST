0d8ab732e55f726ce09bc110d760f845ca5b6b7d
Michalina Janowska <janowska.michalina208@gmail.com>
2025-09-02 15:40:45 +0200

    header finish....

--- docs/scripts/hamburger.js
+++ docs/scripts/hamburger.js
@@ -1,10 +1,20 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const hamburger = document.querySelector('.header__hamburger');
-  const nav = document.querySelector('.header__nav');
+document.addEventListener("DOMContentLoaded", () => {
+  const hamburger = document.querySelector(".header__hamburger");
+  const navMenu = document.querySelector(".header__menu");
 
-  hamburger.addEventListener('click', () => {
-    const expanded = hamburger.getAttribute('aria-expanded') === 'true';
-    hamburger.setAttribute('aria-expanded', !expanded);
-    nav.classList.toggle('header__nav--active');
-  });
+  if (hamburger && navMenu) {
+    hamburger.addEventListener("click", () => {
+      const expanded = hamburger.getAttribute("aria-expanded") === "true" || false;
+      hamburger.setAttribute("aria-expanded", !expanded);
+
+      hamburger.classList.toggle("is-active");
+      navMenu.classList.toggle("active");
+
+      if (navMenu.classList.contains("active")) {
+        document.body.style.overflow = "hidden";
+      } else {
+        document.body.style.overflow = "";
+      }
+    });
+  }
 });
