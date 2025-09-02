efc84e5608446f26a2f6d07e747fb94002bf0eaa
Michalina Janowska <janowska.michalina208@gmail.com>
2025-09-02 18:16:24 +0200

    tofay finish

--- docs/scripts/postLoad.js
+++ docs/scripts/postLoad.js
@@ -1,34 +1,43 @@
 function loadNewsUpdates() {
     const data = {
         posts: [
-            
             {
-                image: "img/ogrod4.jpg",
-                title: "Main Post Title",
-                description: "This is the description for the main post.",
-                date: "2023-10-01",
-            },
-            {
-                image: "img/ogrod5.jpg",
-                title: "Other Post 1 Title",
-                description:
-                    "This is the description for the first other post.",
-                date: "2023-09-30",
-            },
-            {
-                image: "img/ogrod6.jpg",
-                title: "Other Post 2 Title",
-                description:
-                    "This is the description for the second other post.",
-                date: "2023-09-29",
-            },
-            {
-                image: "img/ogrod7.jpg",
-                title: "Other Post 3 Title",
-                description:
-                    "This is the description for the third other post.",
-                date: "2023-09-28",
-            },
+    image: "img/ogrod8.jpg",
+    title: "Nowy ogród w Warszawie",
+    description: "Dzisiaj zakończyliśmy aranżację przydomowego ogrodu w Warszawie – klient był zachwycony nową rabatą kwiatową i altanką.",
+    date: "2025-09-02",
+},
+{
+    image: "img/ogrod5.jpg",
+    title: "Taras z ogrodem w Krakowie",
+    description: "Stworzyliśmy nowoczesny taras z zielonymi donicami w Krakowie. Sezon letni w pełni, a rośliny już pięknie kwitną!",
+    date: "2025-09-01",
+},
+{
+    image: "img/ogrod6.jpg",
+    title: "Ogród przy firmie w Poznaniu",
+    description: "Dzisiaj wykonaliśmy kompleksową pielęgnację ogrodu przy firmie w Poznaniu – nowy system podlewania działa bez zarzutu.",
+    date: "2025-08-30",
+},
+{
+    image: "img/ogrod7.jpg",
+    title: "Mały ogródek w Gdańsku",
+    description: "Zmieniliśmy aranżację małego ogródka miejskiego w Gdańsku – klient dostał dodatkowe miejsce na warzywa i zioła.",
+    date: "2025-08-29",
+},
+{
+    image: "img/ogrod10.webp",
+    title: "Ogród pokazowy w Łodzi",
+    description: "Dzisiaj zakończyliśmy ogród pokazowy w Łodzi. Ścieżki kamienne i oczko wodne przyciągają uwagę każdego przechodnia.",
+    date: "2025-08-28",
+},
+{
+    image: "img/ogrod4.jpg",
+    title: "Ogród przy domu w Szczecinie",
+    description: "Stworzyliśmy piękny ogród przydomowy w Szczecinie z dużą ilością roślin kwitnących – idealne miejsce na weekendowe relaksowanie się.",
+    date: "2025-08-27",
+}
+
             
         ],
     };
